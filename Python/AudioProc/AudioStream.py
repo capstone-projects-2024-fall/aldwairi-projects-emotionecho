72474c6d79aeb5df6e05bb41f9b66b692ebe0d54
fbassill <tun39698@temple.edu>
2024-10-21 21:46:33 -0400

    Circ buffer impl & Manager changes

--- Python/AudioProc/AudioStream.py
+++ Python/AudioProc/AudioStream.py
@@ -1,4 +1,49 @@
-
+import numpy as np
+import threading
 
 class AudioStream:
-    pass
+    
+    def __init__(self, buffSize: int, chunkSize: int):
+        self.buffSize = buffSize
+        self.chunkSize = chunkSize
+        
+        self.buffer = np.zeros(self.buffSize, dtype=np.int16)
+        self.wPos = 0
+        self.rPos = 0
+        self.size = 0
+        self.lock = threading.Lock()
+
+
+    def write(self, data):
+        with self.lock:
+            if len(data) + self.size > self.buffSize:
+                raise ValueError("Buffer overflow")
+
+            endPos = self.wPos + len(data)
+            if endPos <= self.buffSize:
+                self.buffer[self.wPos:endPos] = data
+            else:
+                endSplit = self.buffSize - self.wPos
+                self.buffer[self.wPos:] = data[:endSplit]
+                self.buffer[:len(data) - endSplit] = data[endSplit:]
+            
+            self.wPos = (self.wPos + len(data)) % self.buffSize
+            self.size += len(data)
+
+    def read(self):
+        with self.lock:
+            if self.size < self.chunkSize:
+                raise ValueError("Not enough data in buffer")
+
+            endPos = self.rPos + self.chunkSize
+            if endPos <= self.buffSize:
+                data = self.buffer[self.rPos:endPos]
+            else:
+                endSplit = self.buffSize - self.rPos
+                data = np.concatenate((self.buffer[self.rPos:], self.buffer[:self.chunkSize - endSplit]))
+            
+            self.rPos = (self.rPos + self.chunkSize) % self.buffSize
+            self.size -= self.chunkSize
+
+        return data
+    
--- Python/AudioProc/Manager.py
+++ Python/AudioProc/Manager.py
@@ -1,26 +1,40 @@
-import AudioStream
-import SegmentStream
-import AudioReceiver
-import AudioProcessor
+from AudioStream import AudioStream
+from AudioProcessor import AudioProcessor
+from AudioSegment import AudioSegment
 import threading
+
+BUFFER_SIZE = 8192 # 8 KB buffer: ~185ms at 44.1kHz 16bit mono
 
 class Manager:
 
-    def __init__(self):
-        self.audioStream = AudioStream()
-        self.segmentStream = SegmentStream()
+    def __init__(self, sampleRatePerSec: int, bitDepth: int, channelCount: int, segDurationMS: int):
         self.stopEvnt = threading.Event()
+        self.sampleRatePerSec = sampleRatePerSec
+        self.bitDepth = bitDepth
+        self.channelCount = channelCount
+        self.segDurationMS = segDurationMS
+
+        self.audioStream = AudioStream(BUFFER_SIZE, self.calcChunkSize())
+        #self.segmentStream = SegmentStream
 
     def startAudioProc(self):
-        self.audioReceiver = AudioReceiver()
-        self.audioProcessor = AudioProcessor()
-
-        self.audioReceiver.start()
+        self.audioProcessor = AudioProcessor(self, self.stopEvnt)
         self.audioProcessor.start()
 
     def stopAudioProc(self):
         self.stopEvnt.set()
-
-        self.audioReceiver.join()
         self.audioProcessor.join()
 
+    def addPCMData(self):
+        pass
+
+    def getProcessedSegment(self) -> AudioSegment:
+        #This method will need updating
+        if self.segmentStream.isEmpty():
+            return None
+        else:
+            return self.segmentStream.get()
+        
+    def calcChunkSize(self) -> int:
+        samples = self.sampleRatePerSec * self.segDurationMS * .001
+        return samples * self.channelCount * (self.bitDepth / 8)
