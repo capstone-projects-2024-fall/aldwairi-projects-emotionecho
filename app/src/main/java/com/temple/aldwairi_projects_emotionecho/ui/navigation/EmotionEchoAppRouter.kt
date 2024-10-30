46a389f5bb579c74e11bbd3a08fed9ebe52ff37e
Francis R Scallatino <FrancisRScallatino@gmail.com>
2024-10-29 21:06:53 -0400

    make code a bit more concise

--- app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/navigation/EmotionEchoAppRouter.kt
+++ app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/navigation/EmotionEchoAppRouter.kt
@@ -1,6 +1,6 @@
 package com.temple.aldwairi_projects_emotionecho.ui.navigation
 
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Default
 import androidx.compose.material.icons.filled.Hearing
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
@@ -10,11 +10,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen(var title: String, var icon: ImageVector, var screenRoute: String, val showBottomBar: Boolean, var onClick: () -> Unit){
-    object LoginScreen: Screen("Login", Icons.Default.Person, "login", false, {})
-    object RegisterSCreen: Screen("Register", Icons.Default.Person, "register", false, {})
-    object HomeScreen: Screen("Home", Icons.Default.Home, "home", true, {})
-    object PracticeModeScreen: Screen("PracticeMode", Icons.Default.Psychology, "practice", true, {})
-    object RealTimeMode: Screen("RealTimeMode", Icons.Default.Hearing, "real_time", true, {})
+    object LoginScreen: Screen("Login", Default.Person, "login", false, {})
+    object RegisterSCreen: Screen("Register", Default.Person, "register", false, {})
+    object HomeScreen: Screen("Home", Default.Home, "home", true, {})
+    object PracticeModeScreen: Screen("PracticeMode", Default.Psychology, "practice", true, {})
+    object RealTimeMode: Screen("RealTimeMode", Default.Hearing, "real_time", true, {})
 }
 
 sealed class App{
