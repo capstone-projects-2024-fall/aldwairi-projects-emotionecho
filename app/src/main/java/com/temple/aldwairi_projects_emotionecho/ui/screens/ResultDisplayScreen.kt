30a3d83f044da95905e8b15f87235bd8fa9b66cb
Francis R Scallatino <tup31915@temple.edu>
2024-10-31 15:19:06 -0400

    condense and wrap content
    This is merely data we want to display, we do not want this to take up the entire screen. So intestead of fillMaxSize() we want wrapContent().

--- app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/screens/ResultDisplayScreen.kt
+++ app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/screens/ResultDisplayScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -39,19 +41,16 @@ fun PieChartWithLegend(floatList: List<Float>) {
     val data = processFloatList(floatList)
     Surface(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
+            .wrapContentSize()
+            .padding(10.dp)
     ){
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ){
+        Row {
             PieChart(data = data)
-            Spacer(modifier = Modifier.height(16.dp))
-            data.forEach { slice ->
-                LegendItem(color = slice.color, text = slice.label)
+            Spacer(modifier = Modifier.width(5.dp))
+            Column{
+                data.forEach { slice ->
+                    LegendItem(color = slice.color, text = slice.label)
+                }
             }
         }
     }
@@ -59,13 +58,12 @@ fun PieChartWithLegend(floatList: List<Float>) {
 
 @Composable
 fun PieChart(
-    data: List<PieChartData>,
-    modifier: Modifier = Modifier.size(150.dp),
+    data: List<PieChartData>
 ) {
     val totalValue = data.sumOf { it.value.toDouble() }.toFloat()
     var startAngle = 0f
 
-    Canvas(modifier = modifier.fillMaxSize()) {
+    Canvas(modifier = Modifier.size(150.dp)) {
         data.forEach { slice ->
             val sweepAngle = (slice.value / totalValue) * 360f
             drawArc(
@@ -87,6 +85,7 @@ fun LegendItem(color: Color, text: String) {
         Canvas(modifier = Modifier.size(16.dp)) {
             drawCircle(color = color)
         }
+        Spacer(Modifier.width(5.dp))
         Text(text = text, style = MaterialTheme.typography.bodyMedium)
     }
 }
