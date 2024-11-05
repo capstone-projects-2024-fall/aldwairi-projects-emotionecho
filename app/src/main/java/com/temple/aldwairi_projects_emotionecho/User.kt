d257e6efea533e530e0c331f2fd009452c03287f
RaisedDown <123016407+RaisedDown@users.noreply.github.com>
2024-11-04 20:58:30 -0500

    added basic sign in and sign out

--- app/src/main/java/com/temple/aldwairi_projects_emotionecho/User.kt
+++ app/src/main/java/com/temple/aldwairi_projects_emotionecho/User.kt
@@ -1,4 +1,20 @@
 package com.temple.aldwairi_projects_emotionecho
 
-class User {
-}
+class User (
+    val userID: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val email: String? = null
+    ) {
+
+        @Override
+        override fun toString(): String = userID + "\n" + firstName + "\n" + lastName + "\n" + email
+
+        companion object{
+            public val USER_TABLE = "Users"
+            public val USERID = "userID"
+            public val FIRST_NAME = "firstName"
+            public val LAST_NAME = "lastName"
+            public val EMAIL_ADDRESS = "email"
+        }
+    }
--- app/src/main/java/com/temple/aldwairi_projects_emotionecho/WAVcompressor.kt
+++ app/src/main/java/com/temple/aldwairi_projects_emotionecho/WAVcompressor.kt
@@ -1,4 +1,6 @@
 package com.temple.aldwairi_projects_emotionecho
 
 class WAVcompressor {
+
+    //Will fill in with code to convert WAV file into FLAC before sending to Firebase
 }
