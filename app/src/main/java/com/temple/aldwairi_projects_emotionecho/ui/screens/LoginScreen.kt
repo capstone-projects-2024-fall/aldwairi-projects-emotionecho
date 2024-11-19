d070a9082772b05f700c71023ce95af0b6d43594
Francis R Scallatino <tup31915@temple.edu>
2024-11-18 21:15:02 -0500

    add new custom devider and some code for a hyperlinked text to switch to signup screen (not finished yet)

--- app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/screens/LoginScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -16,12 +17,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.temple.aldwairi_projects_emotionecho.ui.components.CustomButton
+import com.temple.aldwairi_projects_emotionecho.ui.components.CustomDividerWithText
 import com.temple.aldwairi_projects_emotionecho.ui.components.CustomTextInput
 import com.temple.aldwairi_projects_emotionecho.ui.theme.TempleCherryRed
+
+const val SIGNUP_SCREEN = "SIGNUP_SCREEN"
 
 @Composable
 fun LogInScreen(
@@ -55,8 +64,34 @@ fun LogInScreen(
                 text = "Login",
                 brush = listOf(Color.Black, TempleCherryRed)
             ) { TODO() }
+            DividerDefaults.apply {
+                color.blue
+                Thickness.div(1.dp)
+            }
+            CustomDividerWithText()
         }
     }
+}
+
+fun getAnnotatedString(): AnnotatedString {
+    return buildAnnotatedString {
+        append("Don't have an account? ")
+        pushStringAnnotation(
+            tag = SIGNUP_SCREEN,
+            "takes you to signup screen"
+        )
+        withStyle(
+            style = SpanStyle(
+                color = Color.Blue,
+                textDecoration = TextDecoration.Underline
+            )
+        ){
+            append("Sign up here")
+        }
+        pop()
+        append(" and create your account today!")
+    }
+
 }
 
 @Preview(showBackground = true)
