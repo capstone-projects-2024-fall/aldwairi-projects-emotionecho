c6260206d7754940629d56561d041c8c3787f19a
Francis R Scallatino <tup31915@temple.edu>
2024-11-21 00:22:22 -0500

    implement some of the new Material color schemes
    changed the button so it no longer fades from left to right
    
    included some implementation of the new color scheme to allow for light and dark mode changes
    
    also made buttons in the BottomBar easier to read

--- app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/components/CustomButton.kt
+++ app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/components/CustomButton.kt
@@ -10,31 +10,26 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.temple.aldwairi_projects_emotionecho.ui.theme.TempleCherryRed
-import com.temple.aldwairi_projects_emotionecho.ui.theme.TempleYellow
+import com.temple.aldwairi_projects_emotionecho.ui.theme.AldwairiprojectsemotionechoTheme
 
 /**
  * Creates a custom Button Composable
  * @param text the text you want shown on the button
- * @param brush requires a List of at least 2 Colors, used to make the button look more pretty
  * @param onClick callback function for Button's onclick property
  */
 @Composable
 fun CustomButton(
     text: String = "Button",
-    brush: List<Color> = listOf(Black, TempleYellow, TempleCherryRed, TempleCherryRed,
-        TempleCherryRed, TempleYellow, Black), // I know these default colors are kinda ridiculous, will change later
     onClick: () -> Unit
 ) {
     Button(
@@ -49,7 +44,7 @@ fun CustomButton(
             .fillMaxWidth()
             .heightIn(48.dp)
             .background(
-                brush = Brush.horizontalGradient(brush),
+                color = MaterialTheme.colorScheme.primaryContainer,
                 shape = RoundedCornerShape(25.dp)
             ),
             contentAlignment = Alignment.Center
@@ -57,7 +52,7 @@ fun CustomButton(
             Text(text = text,
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Bold,
-                color = Color.White
+                color = MaterialTheme.colorScheme.onPrimaryContainer
             )
         }
     }
@@ -66,5 +61,7 @@ fun CustomButton(
 @Preview
 @Composable
 fun PreviewButton(){
-    CustomButton("This is a Button") { }
+    AldwairiprojectsemotionechoTheme(darkTheme = true) {
+        CustomButton("This is a Button") { }
+    }
 }
