1493f92fc4d553f35003c22283423086b118a2e6
Francis R Scallatino <tup31915@temple.edu>
2024-11-27 22:02:18 -0500

    use new tutorial component

--- app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/navigation/EmotionEchoApp.kt
+++ app/src/main/java/com/temple/aldwairi_projects_emotionecho/ui/navigation/EmotionEchoApp.kt
@@ -1,14 +1,8 @@
 package com.temple.aldwairi_projects_emotionecho.ui.navigation
 
 import android.content.Context
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -19,14 +13,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.temple.aldwairi_projects_emotionecho.ui.components.CustomNavBar
+import com.temple.aldwairi_projects_emotionecho.ui.components.TutorialNavBarItemBox
 import com.temple.aldwairi_projects_emotionecho.ui.theme.AldwairiprojectsemotionechoTheme
 
 @Composable
@@ -43,7 +36,6 @@ fun EmotionEchoApp(context: Context){
             Scaffold(
                 bottomBar = { CustomNavBar(navController, navBarItemLocationOnScreen) }
             ) { innerPadding ->
-
                 NavigationGraph(
                     modifier = Modifier.padding(innerPadding),
                     navController = navController,
@@ -60,27 +52,12 @@ fun EmotionEchoApp(context: Context){
 
 @Composable
 fun TutorialOverlay(navBarItemLocation: Map<String, LayoutCoordinates>) {
-    val targetLocation = navBarItemLocation[Screen.PracticeModeScreen.screenRoute]?.boundsInRoot()
+    val targetLocation = navBarItemLocation[Screen.RealTimeModeScreen.screenRoute]?.boundsInRoot()
     val density = LocalContext.current.resources.displayMetrics.density
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0x88000000)) // semi-transparent overlay
-    ){
-        targetLocation?.let { bounds ->
-            Box(
-                modifier = Modifier
-                    .offset(
-                        x = (bounds.left/density).dp,
-                        y = (bounds.top/density).dp
-                    )
-                    .size((bounds.width/density).dp,(bounds.height/density).dp) // Replace with proper highlight size
-                    .background(
-                        color = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.5f) ,
-                        shape = RoundedCornerShape(50.dp)) // Highlight example
-            )
-        }
+    targetLocation?.let {bounds ->
+        TutorialNavBarItemBox(bounds, "this is a step with a really long description that just doesn't seem to end for some reaosn")
     }
+
 }
 
 
